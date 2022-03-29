44f701669203a09fc274d1bc8aa352f4d5b15172
Milan Lakhani <milanlakhaniaiml@gmail.com>
2022-03-28 21:41:59 -0400

    Added theming colors to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,32 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          dark: "var(--color-text-dark)",
+          light: "var(--color-text-light)",
+        }
+      },
+      backgroundColor: {
+        skin: {
+          fill: "var(--color-background)",
+          filldark: "var(--color-background-dark)",
+          button: "var(--color-button)",
+          buttonhover: "var(--color-button-hover)",
+        }
+      },
+      borderColor: {
+        skin: {
+          dark: "var(--color-text-dark)",
+          light: "var(--color-text-light)", 
+        }
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
